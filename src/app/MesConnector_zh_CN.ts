b41980462a73c984e7ece4ec2309276392fa6d7a
Sanlo <sanlozhang@skiff.com>
2024-11-26 09:22:34 +0800

    update translation file

--- src/app/MesConnector_zh_CN.ts
+++ src/app/MesConnector_zh_CN.ts
@@ -1,3 +1,411 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="zh_CN"></TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>MESConnectorClient</name>
+    <message>
+        <location filename="mesconnectorclient.ui" line="14"/>
+        <location filename="mesconnectorclient.cpp" line="184"/>
+        <location filename="mesconnectorclient.cpp" line="272"/>
+        <location filename="mesconnectorclient.cpp" line="278"/>
+        <location filename="mesconnectorclient.cpp" line="307"/>
+        <location filename="mesconnectorclient.cpp" line="382"/>
+        <source>MES Connector Client</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mesconnectorclient.ui" line="40"/>
+        <source>System Log</source>
+        <translation>系统日志</translation>
+    </message>
+    <message>
+        <location filename="mesconnectorclient.ui" line="61"/>
+        <source>Clear All</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mesconnectorclient.ui" line="68"/>
+        <source>Copy to Paste</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mesconnectorclient.ui" line="89"/>
+        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;meta charset=&quot;utf-8&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+hr { height: 1px; border-width: 0; }
+li.unchecked::marker { content: &quot;\2610&quot;; }
+li.checked::marker { content: &quot;\2612&quot;; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Segoe UI&apos;; font-size:9pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;br /&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mesconnectorclient.ui" line="114"/>
+        <source>Bosch</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mesconnectorclient.ui" line="137"/>
+        <source>Settings...</source>
+        <translation>设置...</translation>
+    </message>
+    <message>
+        <location filename="mesconnectorclient.ui" line="162"/>
+        <source>Part Validation</source>
+        <translation type="unfinished">零件验证</translation>
+    </message>
+    <message>
+        <location filename="mesconnectorclient.ui" line="170"/>
+        <source>Part Identifier</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mesconnectorclient.ui" line="183"/>
+        <source>Validate</source>
+        <translation>验证零件</translation>
+    </message>
+    <message>
+        <location filename="mesconnectorclient.ui" line="190"/>
+        <source>Start Inspect</source>
+        <translation>开始检测</translation>
+    </message>
+    <message>
+        <location filename="mesconnectorclient.ui" line="223"/>
+        <location filename="mesconnectorclient.ui" line="354"/>
+        <location filename="mesconnectorclient.ui" line="448"/>
+        <location filename="mesconnectorclient.ui" line="469"/>
+        <source>TextLabel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mesconnectorclient.ui" line="231"/>
+        <source>Measure Data Transmit</source>
+        <translation>测量数据传输</translation>
+    </message>
+    <message>
+        <location filename="mesconnectorclient.ui" line="239"/>
+        <source>Part List</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mesconnectorclient.ui" line="271"/>
+        <source>Start Transmit</source>
+        <translation>开始传输测量数据</translation>
+    </message>
+    <message>
+        <location filename="mesconnectorclient.ui" line="295"/>
+        <source>Automate Data Transmit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mesconnectorclient.ui" line="337"/>
+        <source>MES Connection</source>
+        <translation>MES 连接</translation>
+    </message>
+    <message>
+        <location filename="mesconnectorclient.ui" line="363"/>
+        <source>IP</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mesconnectorclient.ui" line="375"/>
+        <location filename="mesconnectorclient.ui" line="394"/>
+        <source>.....</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mesconnectorclient.ui" line="382"/>
+        <source>Port</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mesconnectorclient.ui" line="401"/>
+        <location filename="mesconnectorclient.ui" line="476"/>
+        <source>Connect</source>
+        <translation>测试连接</translation>
+    </message>
+    <message>
+        <location filename="mesconnectorclient.ui" line="408"/>
+        <source>Disconnect</source>
+        <translation>断开连接</translation>
+    </message>
+    <message>
+        <location filename="mesconnectorclient.ui" line="431"/>
+        <source>DataLoop Connection</source>
+        <translation type="unfinished">DataLoop 连接</translation>
+    </message>
+    <message>
+        <location filename="mesconnectorclient.ui" line="457"/>
+        <source>DataLoop API</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mesconnectorclient.cpp" line="261"/>
+        <source>Waiting for user input.
+Please scan part ID with BAR scanner</source>
+        <translation type="unfinished">等待用户输入。 使用扫码枪对待测零件扫码</translation>
+    </message>
+    <message>
+        <location filename="mesconnectorclient.cpp" line="39"/>
+        <source>Mes Server is Disconnected.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mesconnectorclient.cpp" line="84"/>
+        <source>Cannot read file %1: %2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mesconnectorclient.cpp" line="96"/>
+        <source>Parse error in file %1:
+
+%2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mesconnectorclient.cpp" line="142"/>
+        <location filename="mesconnectorclient.cpp" line="383"/>
+        <source>Cannot write file %1:
+%2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mesconnectorclient.cpp" line="179"/>
+        <source>The connection is refused by the peerMake sure the server is running, and check the ip and port settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mesconnectorclient.cpp" line="185"/>
+        <source>The following error occurred: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mesconnectorclient.cpp" line="272"/>
+        <source>Server not connected!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mesconnectorclient.cpp" line="278"/>
+        <source>Socket Error!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mesconnectorclient.cpp" line="307"/>
+        <source>Server has&apos;t been connected!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mesconnectorclient.cpp" line="341"/>
+        <source>%1 %2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mesconnectorclient.cpp" line="341"/>
+        <source>yyyy-MM-dd hh:mm:ss</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mesconnectorclient.cpp" line="350"/>
+        <source>Mes Server is Connected.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mesconnectorclient.cpp" line="365"/>
+        <source>Diconnected with MES server.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mesconnectorclient.cpp" line="412"/>
+        <source>PartID = %1, start validate and start PolyWorks</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mesconnectorclient.cpp" line="414"/>
+        <source>ID:%1 is recevied</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="mesconnectorclient.cpp" line="400"/>
+        <source>XML file is writed!</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Settings</name>
+    <message>
+        <location filename="settings.ui" line="14"/>
+        <source>Settings</source>
+        <translation>设置</translation>
+    </message>
+    <message>
+        <location filename="settings.ui" line="46"/>
+        <source>General</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="settings.ui" line="63"/>
+        <source>Theme</source>
+        <translation>主题</translation>
+    </message>
+    <message>
+        <location filename="settings.ui" line="73"/>
+        <source>Light</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="settings.ui" line="80"/>
+        <source>Dark</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="settings.ui" line="87"/>
+        <source>Auto</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="settings.ui" line="107"/>
+        <source>Language</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="settings.ui" line="124"/>
+        <source>English</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="settings.ui" line="129"/>
+        <source>Chinese</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="settings.ui" line="163"/>
+        <source>Part Recevied</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="settings.ui" line="176"/>
+        <location filename="settings.ui" line="209"/>
+        <source>...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="settings.ui" line="196"/>
+        <source>Part Processed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="settings.ui" line="240"/>
+        <source>Connection</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="settings.ui" line="257"/>
+        <source>MES IP:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="settings.ui" line="280"/>
+        <source>Port: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="settings.ui" line="319"/>
+        <source>DataLoop API Entry</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="settings.ui" line="345"/>
+        <source>DataLoop Token</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="settings.ui" line="371"/>
+        <source>......</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="settings.ui" line="378"/>
+        <source>Test API</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="settings.ui" line="409"/>
+        <source>Workstation Infomation</source>
+        <translation>工位信息</translation>
+    </message>
+    <message>
+        <location filename="settings.ui" line="440"/>
+        <source>Application</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="settings.ui" line="488"/>
+        <source>Station No.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="settings.ui" line="539"/>
+        <source>Line No.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="settings.ui" line="587"/>
+        <source>Functional No.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="settings.ui" line="638"/>
+        <source>Station Index</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="settings.ui" line="686"/>
+        <source>Tool Position</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="settings.ui" line="743"/>
+        <source>Working Position</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="settings.ui" line="791"/>
+        <source>Process Name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="settings.ui" line="842"/>
+        <source>Process No.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="settings.ui" line="923"/>
+        <source>OK</source>
+        <translation>确认</translation>
+    </message>
+    <message>
+        <location filename="settings.ui" line="930"/>
+        <source>Cancel</source>
+        <translation>取消</translation>
+    </message>
+    <message>
+        <location filename="settings.ui" line="937"/>
+        <source>Apply</source>
+        <translation>应用</translation>
+    </message>
+    <message>
+        <location filename="settings.cpp" line="107"/>
+        <source>Select Folder for Part Recevied</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="settings.cpp" line="125"/>
+        <source>Select Folder for Part Processed</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
